38194008d7ab058f22dc75a1c8a69d1928ac66bc
Joy Ram <joyram2015@gmail.com>
2023-10-08 11:48:52 +0600

    added new file (books.json,jobs.json,posts.json)  into data folder and import in the db.json

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
 const tags = require("./data/tags.json");
+const books = require("./data/books.json");
+const posts = require("./data/posts.json");
+const jobs = require("./data/jobs.json");
 
 module.exports = {
   todos,
@@ -10,4 +13,7 @@ module.exports = {
   videos2,
   transactions,
   tags,
+  books,
+  posts,
+  jobs,
 };
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
   console.log(`JSON Server is running on http://localhost:${port}`);
